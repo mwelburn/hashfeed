935fdbf4cc0fa6822ddf22591af0cdbc59d09a82
br0nc080 <mwelbu2@gmail.com>
2011-10-09 16:17:27 -0500

    committing default setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hashfeed::Application.config.secret_token = '47d09d246361f43fbe93041ddbfd416ce0e1b5d43fb21eb7eadf959a0f87814d980dc6f5662581aa0edbcfaa8decc75591e83f32af3930d97f8f71ad0a15174b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hashfeed::Application.config.session_store :cookie_store, :key => '_hashfeed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hashfeed::Application.config.session_store :active_record_store
